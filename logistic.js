6ee04884e08c3b35bd3f21677a046cb4fa44cce6
Gerardo <luis-godfrey@hotmail.com>
2019-08-14 11:21:22 -0500

    linea lista

--- logistic.js
+++ logistic.js
@@ -1,31 +1,88 @@
-///////////////////////77
+// alpha = 0.0005;
+// w = [0,1,1];
+// y = [0,0];
+// x = [0,9];
+// sumAprox = [0,0,0];
+// z = 0 ;
+// epo = 5;
 
-alpha = 0.3;
-w = [0,1,1];
-y = [0,0];
-x = [0,9];
-sumAprox = [0,0,0];
-z = 0 ;
-
-function prube(){
-    
-    for(let n = 0; n < dataset.length; n++){
+// function prube(){
+//     for(let n = 0; n < dataset.length; n++){
         
-        z = w[0] + (w[1] * dataset[n]["px"]) + (w[2] * dataset[n]["py"]);
-        yhat = [ 1 / ( 1 + Math.pow(Math.E,-z)) ];
-        err = - ( dataset[n]["py"] * Math.log(yhat) + (1- (dataset[n]["indice"]) ) * Math.log(1 - yhat) ) ;
+//         z = w[0] + (w[1] * dataset[n]["px"]) + (w[2] * dataset[n]["py"]);
+//         yhat = [ 1 / ( 1 + Math.pow(Math.E,-z)) ];
+//         err = - ( dataset[n]["py"] * Math.log(yhat) + (1- (dataset[n]["indice"]) ) * Math.log(1 - yhat) ) ;
 
-        sumAprox[0] = sumAprox[0] + ( 1 * ((dataset[n]["indice"]) - yhat));
-        sumAprox[1] = sumAprox[1] + (dataset[n]["px"] * ((dataset[n]["indice"]) - yhat));
-        sumAprox[2] = sumAprox[2] + (dataset[n]["py"] * ((dataset[n]["indice"]) - yhat));
-    }
-    w[0] = w[0] + (alpha * sumAprox[0]);
-    w[1] = w[1] + (alpha * sumAprox[1]);
-    w[2] = w[2] + (alpha * sumAprox[2]);
+//         sumAprox[0] = sumAprox[0] + ( 1 * ((dataset[n]["indice"]) - yhat));
+//         sumAprox[1] = sumAprox[1] + (dataset[n]["px"] * ((dataset[n]["indice"]) - yhat));
+//         sumAprox[2] = sumAprox[2] + (dataset[n]["py"] * ((dataset[n]["indice"]) - yhat));
+//     }
+//     w[0] = w[0] + (alpha * sumAprox[0]);
+//     w[1] = w[1] + (alpha * sumAprox[1]);
+//     w[2] = w[2] + (alpha * sumAprox[2]);
 
-    y[0] = ( (x[0] * w[1]) + w[0]) * ( -1 / w[2]);
-    y[1] = ( (x[1] * w[1]) + w[0]) * ( -1 / w[2]);
-      
-    
-    document.getElementById("showAnswer1").innerHTML = err;
-}
+//     y[0] = ( (x[0] * w[1]) + w[0]) * ( -1 / w[2]);
+//     y[1] = ( (x[1] * w[1]) + w[0]) * ( -1 / w[2]);     
+// }
+
+// function epochs(){
+//     if(dataset.length >=2){
+//         for(let e = 0; e < epo; e++ ){
+//             w = [0,1,1];
+//             for(let n = 0; n < dataset.length; n++){
+//                 sumAprox = [0,0,0];
+//                 z = w[0] + (w[1] * dataset[n]["px"]) + (w[2] * dataset[n]["py"]);
+//                 yhat = [ 1 / ( 1 + Math.pow(Math.E,(z * -1))) ];
+
+//                 sumAprox[0] = sumAprox[0] + ( 1 * ((dataset[n]["indice"]) - yhat));
+//                 sumAprox[1] = sumAprox[1] + (dataset[n]["px"] * ((dataset[n]["indice"]) - yhat));
+//                 sumAprox[2] = sumAprox[2] + (dataset[n]["py"] * ((dataset[n]["indice"]) - yhat));
+//             }
+//             w[0] = w[0] + (alpha * sumAprox[0]);
+//             w[1] = w[1] + (alpha * sumAprox[1]);
+//             w[2] = w[2] + (alpha * sumAprox[2]);    
+//         }
+//         y[0] = ( (x[0] * w[1]) + w[0]) * ( -1 / w[2]);
+//         y[1] = ( (x[1] * w[1]) + w[0]) * ( -1 / w[2]);
+//     }
+// }
+
+// function epochs(){
+//     for(let n = 0; n < dataset.length; n++){
+//         w = [0,1,1];
+//         for(let e = 0; e < epo; e++){
+//             sumAprox = [0,0,0];
+//             z = w[0] + (w[1] * dataset[n]["px"]) + (w[2] * dataset[n]["py"]);
+//             yhat = [ 1 / ( 1 + Math.pow(Math.E,z)) ];
+
+//             sumAprox[0] = sumAprox[0] + ( 1 * ( yhat - (dataset[n]["indice"])));
+//             sumAprox[1] = sumAprox[1] + (dataset[n]["px"] * ( yhat - (dataset[n]["indice"]) ));
+//             sumAprox[2] = sumAprox[2] + (dataset[n]["py"] * ( yhat - (dataset[n]["indice"])));
+//         }
+//         w[0] = w[0] - (alpha * sumAprox[0]);
+//         w[1] = w[1] - (alpha * sumAprox[1]);
+//         w[2] = w[2] - (alpha * sumAprox[2]);
+        
+//         y[0] = ( ((x[0] * -1) * w[1]) + (w[0] * -1)) * ( 1 / w[2]);
+//         y[1] = ( ((x[1] * -1) * w[1]) + (w[0] * -1)) * ( 1 / w[2]); 
+//     }
+// }
+
+// function prube(){
+//     for(let n = 0; n < dataset.length; n++){
+        
+//         z = w[0] + (w[1] * dataset[n]["px"]) + (w[2] * dataset[n]["py"]);
+//         yhat = [ 1 / ( 1 + Math.pow(Math.E,z)) ];
+//         err = - ( dataset[n]["py"] * Math.log(yhat) + (1- (dataset[n]["indice"]) ) * Math.log(1 - yhat) ) ;
+
+//         sumAprox[0] = sumAprox[0] + ( 1 * ( yhat - (dataset[n]["indice"])));
+//         sumAprox[1] = sumAprox[1] + (dataset[n]["px"] * ( yhat - (dataset[n]["indice"]) ));
+//         sumAprox[2] = sumAprox[2] + (dataset[n]["py"] * ( yhat - (dataset[n]["indice"])));
+//     }
+//     w[0] = w[0] - (alpha * sumAprox[0]);
+//     w[1] = w[1] - (alpha * sumAprox[1]);
+//     w[2] = w[2] - (alpha * sumAprox[2])
+
+//     y[0] = ( ((x[0] * -1) * w[1]) + (w[0] * -1)) * ( 1 / w[2]);
+//     y[1] = ( ((x[1] * -1) * w[1]) + (w[0] * -1)) * ( 1 / w[2]);     
+// }
--- main.js
+++ main.js
@@ -1,69 +1,112 @@
-var dataset = [];
 var button = document.getElementById("clickme");
 var buttonR = document.getElementById("click_right");
 var countR = 0;
 var countI = 0;
 
-
 button.onclick = function() {
   countI = countI + 1;
-  dataset.push({"indice":0, "px": (Math.random()*5), "py": (Math.random() *5)});
-  button.innerHTML = "Left: " + countI;
+  var posXi = (Math.random()*5);
+  dataset.push({"indice":0, "px": posXi, "py": (Math.random() * 9)- posXi});
+  button.innerHTML = "Yellow: " + countI;
   plot(dataset,svg);
-  prube();
+  epochs();
 };
 
 buttonR.onclick  = function (){
   countR++;
-  dataset.push({"indice":1, "px": (Math.random() * (9 - 3.5) + 3.5), "py": (Math.random() * (9 - 5) + 5) });
-  buttonR.innerHTML = "Right: " + countR;
+  var posXd = Math.random() * (9 - 3.5) + 3.5;
+  dataset.push({"indice":1, "px": (posXd), "py": (Math.random() * (9 - 5) + 5) });
+  buttonR.innerHTML = "Blue: " + countR;
   plot(dataset,svg);
-  prube();
+  epochs();
 };
 
+
+const alpha = 0.0005;
+const x = [0,9];
+const epo = 5;
+var dataset = [];
+var w = [-6.5,1,1];
+var y = [0,0];
+var sumAprox = [0,0,0];
+var z = 0 ;
+var yhat = 0 ;
+
+//DECLARAR EL MARGEN Y CON BASE EN ESO EL TAMAÑO DE LA ALTURA Y ANCHURA
 var margin = {top: 10, right: 30, bottom: 30, left: 60};
 var width = (660 - margin.left - margin.right);
 var height = (600 - margin.top - margin.bottom);
+//QUE COLOR SERA DEPENDIENDO DEL RANGO EN EL QUE ESTE 
 var color = d3.scaleOrdinal().domain(["0","1"]).range(["#fde725ff", "#21908dff"]);
-var svg = d3.select("#dataviz_brushCSS").append("svg").attr("width", width + margin.left + margin.right).attr("height", height + margin.top + margin.bottom).append("g").attr("transform", "translate(" + margin.left + ", " + margin.top + ")");
-//var myCircle = d3.select("#dataviz_brushCSS").append("svg").append("circle").attr("cx", 250).attr("cy", 150).attr("r", 40).attr("fill", "#69a3b2")
+
 //Make an SVG Container
+var svg = d3.select("#dataviz_brushCSS").append("svg").attr("width", width + margin.left + margin.right).attr("height", height + margin.top + margin.bottom).append("g").attr("transform", "translate(" + margin.left + ", " + margin.top + ")");
+
+
+
 
 const plot = (dataset,svg) => {
 
-  var py = d3.scaleLinear().domain([0, 9]).range([ height, 0]);
   
+  var py = d3.scaleLinear().domain([0, 9]).range([ height, 0]);
   svg.append("g").call(d3.axisLeft(py));
-
   var px = d3.scaleLinear().domain([0, 9]).range([ 0, width ]);
-
   svg.append("g").attr("transform", "translate(0," + height + ")").call(d3.axisBottom(px));   
 
   const circle = svg.selectAll('circle').data(dataset);
-  
-  circle.enter().append("circle").attr("cx",function(d) {return  px(dataset[dataset.length - 1]["px"])}) // hasta el 500
+
+  circle.enter().append("circle").attr("cx",function(d) {return  px(dataset[dataset.length - 1]["px"])}) // hasta el 570
     .attr("cy", function(d){return  py (dataset[dataset.length - 1]["py"]) }) // hasta el 500 
     .attr("r",8)
     .style("fill", function (d) { return color(dataset[dataset.length - 1]["indice"]) } )
     .style("opacity", 1)
   ;
-  
-  if(dataset.length >=2){
 
-    svg.selectAll("line").remove();
+  // var line = svg.append("line")
+  // .attr("x1",px(6.5))
+  // .attr("y1",py(0))
+  // .attr("x2",px(0))
+  // .attr("y2",py(6.5))
+  // .attr("stroke-width", 2)
+  // .attr("stroke", function(){
+  //   return '#'+Math.floor(Math.random()*16777215).toString(16);});
 
-    var line = svg.append("line").
-      attr("x1", function (d) {return px(x[0]) }).
-      attr("y1", function (d) {return py(y[0]) }).
-      attr("x2", function (d) {return px(x[1]) }).
-      attr("y2", function (d) {return py(y[1]) }).
-      attr("stroke-width", 2).
-      attr("stroke", "black")
-    ;
-  }
+  svg.selectAll("line").remove();
   
-  
+  var line = svg.append("line").
+  attr("x1", function (d) {return px(x[0]) }).
+  attr("y1", function (d) {return py(y[0]) }).
+  attr("x2", function (d) {return px(x[1]) }).
+  attr("y2", function (d) {return py(y[1]) }).
+  attr("stroke-width", 5).
+  attr("stroke", "red");
+
+  // document.getElementById("showAnswer").innerHTML = px(x[0]);
+
 };
+
+function epochs(){
+  var e;
+  var n;
+  if(dataset.length >=2){
+    console.log(dataset);
+      for(e = 0; e < epo; e++ ){
+        for(n = 0; n < dataset.length; n++){
+          sumAprox = [0,0,0];
+          z = w[0] + (w[1] * ((dataset[n]).px)) + (w[2] * (dataset[n]).py);
+          yhat = [ 1 / ( 1 + Math.pow(Math.E,(z * -1))) ];
+          sumAprox[0] = sumAprox[0] + ( 1 * ((dataset[n]["indice"]) - yhat));
+          sumAprox[1] = sumAprox[1] + (dataset[n].px * ((dataset[n].indice) - yhat));
+          sumAprox[2] = sumAprox[2] + (dataset[n].py * ((dataset[n].indice) - yhat));
+        }
+        w[0] = w[0] + (alpha * sumAprox[0]);
+        w[1] = w[1] + (alpha * sumAprox[1]);
+        w[2] = w[2] + (alpha * sumAprox[2]);    
+      }
+      y[0] = ( (x[0] * w[1]) + w[0]) * ( -1 / w[2]);
+      y[1] = ( (x[1] * w[1]) + w[0]) * ( -1 / w[2]);
+  }
+}
 
 
 
